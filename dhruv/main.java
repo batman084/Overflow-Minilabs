866dc8588cfb0c0463dccdb8a2aeeaacd988e8e1
dhruv <dhruv.sengupta04@gmail.com>
2021-03-26 15:37:40 -0700

    made cookie lab with inheritance

---
+++ dhruv/main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String input = "steve";
+        Joe steve = new Joe("Steve");
+        System.out.println(steve.tostring);
+    }
+
+
+}
