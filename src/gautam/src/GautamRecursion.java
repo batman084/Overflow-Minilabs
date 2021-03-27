ed597f053045057accc1ea5f7d7695acb8ccfe43
batman084 <awesomegautam1@gmail.com>
2021-03-27 03:20:32 -0700

    File structure redone

---
+++ src/gautam/src/GautamRecursion.java
@@ -0,0 +1,10 @@
+package gautam.src;
+
+public class GautamRecursion {
+//minilab for a bunch of recursion examples
+    public static void main (String[] args) { //method that will do all the printing and call methods for every recursion example
+
+    }
+
+}
+
